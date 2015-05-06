482d6e001c5b1839000b92a1c9abc9efeab026e3
hoshitocat <micky12127@gmail.com>
2015-05-06 16:34:47 +0900

    add setup script and .gitignore_global setting

---
+++ setup.sh
@@ -0,0 +1,9 @@
+cp .gitignore_global ~/
+cp .gvimrc           ~/
+cp .vimrc            ~/
+cp .tmux.conf        ~/
+cp .zshenv           ~/
+cp .zshrc            ~/
+git config --global core.excludesfile "~/.gitignore_global"
+
+echo "Finished !!!!!"
