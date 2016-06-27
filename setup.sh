cb809ddda77d56a56bc4874ae65a0c2769d405d5
hoshitocat <ryohei.hoshi@gunosy.com>
2016-06-27 17:09:12 +0900

    Removed old vim settings

--- setup.sh
+++
@@ -1,9 +0,0 @@
-cp .gitignore_global ~/
-cp .gvimrc           ~/
-cp .vimrc            ~/
-cp .tmux.conf        ~/
-cp .zshenv           ~/
-cp .zshrc            ~/
-git config --global core.excludesfile "~/.gitignore_global"
-
-echo "Make a copy completed"
