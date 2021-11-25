d00b4f8e63f50b0f418afe0a288bc2965dcde5df
hoshitocat <hoshitocat@gmail.com>
2021-11-25 12:27:17 +0900

    :hocho: remove unnecessary code

--- shell/zshrc_env.zsh
+++
@@ -1,9 +0,0 @@
-# rbenv
-if [ -d ${HOME}/.rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:${HOME}/.rbenv/shims:${PATH}"
-  eval "$(rbenv init -)"
-fi
-
-# Java
-export JAVA_HOME='/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin'
-export PATH="${JAVA_HOME}:${PATH}"
