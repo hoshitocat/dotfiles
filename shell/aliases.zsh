2950f95135720fa071263f0e044900ca3a071f3a
hoshitocat <hoshitocat@gmail.com>
2026-03-14 10:38:56 +0900

    :wrench: Add the configuration for neovim

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -1,15 +1,6 @@
-if [[ "$OSTYPE" == "linux-gnu" ]] then
-  alias ls='ls --color'
-  alias vi="/bin/vim"
-  alias vim=vi
-fi
-
-if [[ "$OSTYPE" =~ "darwin*" ]] then
-  alias ls='ls -GF'
-  alias vi="/Applications/MacVim.app/Contents/bin/vim"
-  alias vim=vi
-  alias gvim="/Applications/MacVim.app/Contents/bin/gvim"
-fi
+alias ls='ls -GF'
+alias vim=nvim
+alias vi=vim
 
 alias rm='rm -i'
 alias mv='mv -i'
@@ -18,22 +9,5 @@ alias grep='grep --color'
 alias ag='ag --hidden -i'
 alias dc="docker compose"
 alias cat='bat --theme "Visual Studio Dark+"'
-alias zel=zellij
 alias lg=lazygit
 
-function docker_compose_run()
-{
-  if [ -z "$1" ]; then
-    docker-compose help
-    echo "  console, c         Execute rails console"
-    echo "  bash, shell        Execute /bin/bash"
-  else
-    if [ "$1" = "console" ] || [ "$1" = "c" ]; then
-      docker exec -it $(docker-compose ps -q app) /bin/bash -c "export COLUMNS=`tput cols`; export LINES=`tput lines`; rails c"
-    elif [ "$1" = "bash" ]; then
-      docker exec -it $(docker-compose ps -q app) /bin/bash -c "export COLUMNS=`tput cols`; export LINES=`tput lines`; exec bash"
-    else
-      docker-compose $@
-    fi
-  fi
-}
